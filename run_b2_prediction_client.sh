f0cf735f48da5774e71552fd43d4a87a154ca70c
hfyan0@gmail.com <hfyan0@gmail.com>
2016-08-18 22:01:14 +0800

    WIP

--- run_b2_prediction_client.sh
+++ run_b2_prediction_client.sh
@@ -1,6 +1,6 @@
 #!/bin/bash
 
-B2_PREDICTION_CLIENT=/home/qy/Dropbox/nirvana/b2_prediction/b2_prediction_client.py
-CONFIG_LIVE=/home/qy/Dropbox/nirvana/b2_prediction/config_live.ini
+B2_PREDICTION_CLIENT=/home/$(whoami)/Dropbox/nirvana/b2_prediction/b2_prediction_client.py
+CONFIG_LIVE=/home/$(whoami)/Dropbox/nirvana/b2_prediction/config_live.ini
 
 python $B2_PREDICTION_CLIENT $CONFIG_LIVE
--- run_b2_prediction_server.sh
+++ run_b2_prediction_server.sh
@@ -1,6 +1,6 @@
 #!/bin/bash
 
-B2_PREDICTION_SERVER=/home/qy/Dropbox/nirvana/b2_prediction/b2_prediction_server.py
-CONFIG_LIVE=/home/qy/Dropbox/nirvana/b2_prediction/config_live.ini
+B2_PREDICTION_SERVER=/home/$(whoami)/Dropbox/nirvana/b2_prediction/b2_prediction_server.py
+CONFIG_LIVE=/home/$(whoami)/Dropbox/nirvana/b2_prediction/config_live.ini
 
 python $B2_PREDICTION_SERVER $CONFIG_LIVE
