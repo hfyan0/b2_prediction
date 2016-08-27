b5a58396d3086fbad0fa9062019fd19ccb9b85c7
hfyan0@gmail.com <hfyan0@gmail.com>
2016-08-27 16:54:47 +0800

    WIP

--- gen_hist_signals.sh
+++ gen_hist_signals.sh
@@ -2,17 +2,17 @@
 
 CONFIG_HIST=/home/$(whoami)/Dropbox/nirvana/b2_prediction/config_hist.ini
 # CONFIG_HIST=/home/$(whoami)/Dropbox/nirvana/b2_prediction/config_test.ini
-SYMBOLS_FOLDER=$(ls|grep config|grep ini|head -n 1|xargs cat|grep SymbolsFolder|awk -F= '{print $2}')
+SYMBOLS_FOLDER=$(cat $CONFIG_HIST |grep SymbolsFolder|awk -F= '{print $2}')
 GEN_HIST_SGNL_WITH_RPY=/home/$(whoami)/Dropbox/nirvana/b2_prediction/gen_hist_sgnl_with_rpy2.py
 POPULATE_SYM_SCT=/home/$(whoami)/Dropbox/nirvana/b2_prediction/populate_sym_folder.sh
 
-source $POPULATE_SYM_SCT
+source $POPULATE_SYM_SCT $CONFIG_HIST
 
 ###################################################
 # create output folders if they don't already exist
 ###################################################
-ls|grep config|grep ini|head -n 1|xargs cat|grep -v ^#|grep CoefFolder|awk -F= '{print $2}'|xargs mkdir -p
-ls|grep config|grep ini|head -n 1|xargs cat|grep -v ^#|grep ForecastFolder|awk -F= '{print $2}'|xargs mkdir -p
+cat $CONFIG_HIST |grep -v ^#|grep CoefFolder|awk -F= '{print $2}'|xargs mkdir -p
+cat $CONFIG_HIST |grep -v ^#|grep ForecastFolder|awk -F= '{print $2}'|xargs mkdir -p
 
 date
 cd $SYMBOLS_FOLDER
--- populate_sym_folder.sh
+++ populate_sym_folder.sh
@@ -1,13 +1,14 @@
 #!/bin/bash
 
-SYMBOLLIST=$(ls|grep config|grep ini|head -n 1|xargs cat|grep -v ^#|grep SymbolList|awk -F= '{print $2}'|tr ',' ' ')
-SYMBOLS_FOLDER=$(ls|grep config|grep ini|head -n 1|xargs cat|grep -v ^#|grep SymbolsFolder|awk -F= '{print $2}')
+CONFIG_INI=$1
+SYMBOLLIST=$(cat $CONFIG_INI|grep -v ^#|grep SymbolList|awk -F= '{print $2}'|tr ',' ' ')
+SYMBOLS_FOLDER=$(cat $CONFIG_INI|grep -v ^#|grep SymbolsFolder|awk -F= '{print $2}')
 
 [[ ! -d $SYMBOLS_FOLDER ]] && mkdir -p $SYMBOLS_FOLDER
+
+rm -f $SYMBOLS_FOLDER/*
 
 for sym in $SYMBOLLIST
 do
     touch $SYMBOLS_FOLDER/$sym
 done
-
-rm -f $SYMBOLS_FOLDER/*core*
